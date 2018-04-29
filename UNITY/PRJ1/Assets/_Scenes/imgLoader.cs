c4e9d3f3b393d69aac0133fa7ed4c1420e4d8cc6
jamesmcfarland <parallelrealitiesdesign@gmail.com>
2018-04-29 17:35:14 +0100

    Added Intro Scenes and configured build settings

---
+++ UNITY/PRJ1/Assets/_Scenes/imgLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class imgLoader : MonoBehaviour {
+	public Texture aTexture;
+	// Use this for initialization
+	void Start () {
+		
+		 
+
+	}
+	
+	void OnGUI() {
+		if (!aTexture) {
+			Debug.LogError ("Assign a Texture in the inspector.");
+			return;
+		}
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), aTexture, ScaleMode.ScaleToFit, true, 0);
+	}
+	void Update () {
+	 
+	}
+}
